065b2d532eb221c16db96999c393b499f40b9309
Bakhtyiar Klimov <baxtiar00@gmail.com>
2020-12-25 02:24:21 +0300

    Addeed new console.log

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 console.log("The console is working right");
 console.log("This string is evidence of new branch");
 console.log("This string is wrote in main branch by avoiding changes from other branch 777");
+console.log("This string wrote after merging and testing merge conflict case");
