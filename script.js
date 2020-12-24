b0cf3e552d8de79878c8c8e90b09c745c7bbcf0a
Bakhtyiar Klimov <baxtiar00@gmail.com>
2020-12-24 23:10:49 +0300

    Added new console.log
    It is for testing rebase effect

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 console.log("The console is working right");
 console.log("This string is evidence of new branch");
+console.log("This string is wrote in main branch by avoiding changes from other branch 777");
